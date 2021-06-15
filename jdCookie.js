af454d11f5a69a87432b9729859c06c43a740eba
dccice <dccice@163.com>
2021-06-15 17:44:47 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -30,5 +30,5 @@ if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () =
 for (let i = 0; i < CookieJDs.length; i++) {
   if (!CookieJDs[i].match(/pt_pin=(.+?);/) || !CookieJDs[i].match(/pt_key=(.+?);/)) console.log(`\n提示:京东cookie 【${CookieJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: pt_key=xxx;pt_pin=xxx;（分号;不可少）\n`);
   const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['CookieJD' + index] = CookieJDs[i].trim();
+  exports['CookieJD' + index] = CookieJDs[i].trim(); 
 }
